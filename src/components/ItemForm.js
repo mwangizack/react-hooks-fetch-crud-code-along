2db4296815ed1a258b6047c63654fdf1d261ce1f
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-02 14:04:00 +0300

    feat: Add support for an add item feature

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
-function ItemForm() {
-  const [name, setName] = useState("");
-  const [category, setCategory] = useState("Produce");
+function ItemForm({handleAddItem}) {
+  const [formData, setFormData] = useState({
+    name: '',
+    category: 'Produce',
+    isInCart: false,
+  })
+
+  function handleSubmit(e){
+    e.preventDefault()
+    fetch('http://localhost:4000/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => handleAddItem(data))
+    .catch(error => console.log(`Error adding item: ${error}`))
+
+    setFormData({
+      name: '',
+      category: 'Produce',
+      isInCart: false,
+    })
+  }
 
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
           type="text"
           name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={formData.name}
+          onChange={(e) => setFormData({...formData, name: e.target.value})}
         />
       </label>
 
@@ -20,8 +43,8 @@ function ItemForm() {
         Category:
         <select
           name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
+          value={formData.category}
+          onChange={(e) => setFormData({...formData, category: e.target.value})}
         >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { response } from "msw";
 
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+  
+
+  useEffect(() => {
+    fetch('http://localhost:4000/items')
+      .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(error => console.log(`Error fetching items: ${error}`))
+  }, [])
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem])
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,7 +32,7 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm handleAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
